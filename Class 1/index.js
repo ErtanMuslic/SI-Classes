d55644215f536d702853113295bd43fefb95f1f1
--global ertanmuslic@gmail.com <--global ertanmuslic@gmail.com>
2022-10-20 00:07:34 +0200

    First Commit

---
+++ Class 1/index.js
@@ -0,0 +1,32 @@
+// console.log("imamo vezbe i dalje");
+
+// const fs = require('fs');
+// const biblioteka = require('biblioteka');
+const express = require('express');
+// const { read } = require('fs');
+
+// console.log("Radi");
+const app = express();
+const PORT = 3000;
+
+const list = []
+app.use(express.json())
+
+app.get('/',(req,res) => {
+    res.json(list);
+    //res.json({name:'Ertan'}) 
+    //console.log("dobili smo request");
+    //const obj = {name :"Ertan" , age: 21}
+    //res.send(JSON.stringify(obj));
+});
+
+app.post('/' , (req,res) =>{
+    const article = req.body
+    list.push(article)
+    res.status(201).json(article)
+})
+
+app.listen(PORT , () => {
+    console.log(`listening on localhost:${PORT}`)
+});
+
---
+++ Homework-1/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+let list = [];
+
+app.use(express.json())
+
+app.get('/add',(req,res) =>{
+    
+    const sum = list.reduce((prev,curr) => prev + curr ,0);
+    res.json(sum);
+
+
+    // for(let i = 0;i<list.length;i++){
+    //     sum+=list[i];
+    // }
+    // res.json(sum);
+});
+
+app.get('/sort',(req,res) => {
+
+    list.reduce((prev,curr) =>{ list.sort(); } , 0 );
+    res.send(JSON.stringify(list));
+
+
+    // let min = list[0];
+    // for(i = 0;i<list.length;i++){
+    //     if(min > list[i]){
+    //         min = list[i];
+    //     }
+    // }
+    // res.json(min);
+
+})
+
+app.post('/' , (req,res) =>{
+
+    const numbers = req.body.num;
+    list = numbers;
+    res.status(201).json(numbers);
+    // console.log(list);
+
+})
+
+app.listen(PORT, () =>{
+
+    console.log(`listening on localhost:${PORT}`);
+
+})
