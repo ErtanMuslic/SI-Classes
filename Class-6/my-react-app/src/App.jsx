f1f48ff3cce76842d02cf4626a1b1496869593b9
Ertan Muslic <ertanmuslic@gmail.com>
2022-11-19 14:33:09 +0100

    Tiles

--- Class-6/my-react-app/src/App.jsx
+++ Class-6/my-react-app/src/App.jsx
@@ -17,36 +17,47 @@ function App() {
   // },[broj]);
 
 
-  const [joke,setJoke] = useState('');
-  const [isLoading,setisLoading] = useState(false);
-  const [error,setError] = useState(null);
+  // const [joke,setJoke] = useState('');
+  // const [isLoading,setisLoading] = useState(false);
+  // const [error,setError] = useState(null);
 
-  useEffect(() =>{
-    async function f() {
-      setisLoading(true);
-      setError(null);
-      try{
-        const response = await axios.get('https://api.chucknorris.io/jokes/random');
-        setJoke(response.data.value);
-      }
-      catch(e){
-        setError(e);
-      }
-      setisLoading(false);
-    }
-    f();
-  },[])
+  // useEffect(() =>{
+  //   async function f() {
+  //     setisLoading(true);
+  //     setError(null);
+  //     try{
+  //       const response = await axios.get('https://api.chucknorris.io/jokes/random');
+  //       setJoke(response.data.value);
+  //     }
+  //     catch(e){
+  //       setError(e);
+  //     }
+  //     setisLoading(false);
+  //   }
+  //   f();
+  // },[])
 
-  if(error){
-    return <h1>Error se desio: {error}</h1>
+  // if(error){
+  //   return <h1>Error se desio: {error}</h1>
+  // }
+  // if(isLoading){
+  //   return <h1>Loading...</h1>
+  // }
+
+  const images = {
+    0:'assets./dune.png',
+    1:'assets./road.png',
+    2:'assets./water.png'
   }
-  if(isLoading){
-    return <h1>Loading...</h1>
-  }
+
+
+  const [width,setWidth] = useState(3);
+  const [height,setHeight] = useState(4);
+  const [tiles,setTiles] = useState([0,0,0,1,1,1,1,2,2,1,1])
 
  return (
-    <div>
-      {joke}
+    <div style={{display:'inline-grid' ,gridTemplateColumns:'repeat(4,'}}>
+      tiles.map({tile => <img src={images[tile]}/>})
     </div>
   ) 
 }
