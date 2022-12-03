fff3064598223325117910e62873bd6c61937a92
Ertan Muslic <ertanmuslic@gmail.com>
2022-12-03 14:41:33 +0100

    .

--- Class-6/my-react-app/src/App.jsx
+++ Class-6/my-react-app/src/App.jsx
@@ -50,14 +50,57 @@ function App() {
     2:'assets./water.png'
   }
 
+  const TileMap = {
+    0:"red",
+    1:"green",
+    2:"blue",
+    3:"grey",
+    
+  }
 
-  const [width,setWidth] = useState(3);
-  const [height,setHeight] = useState(4);
-  const [tiles,setTiles] = useState([0,0,0,1,1,1,1,2,2,1,1])
+  const path = [
+    {x :0,y:0},
+    {x :1,y:0},
+    {x :1,y:1},
+    {x :1,y:2},
+    {x :2,y:2}
+]
+
+  const [width,setWidth] = useState(4);
+  const [height,setHeight] = useState(3);
+  const [tiles,setTiles] = useState([3,1,1,2,1,1,2,2,1,2,1,1])
+
+  const [playerX,setplayerX] = useState(0);
+  const [playerY,setplayerY] = useState(0);
+  
+
+  useEffect(()=>{
+    setplayerX()
+  })
+
+
+  function getTile(i,j) {
+    return tiles[i * width + j]
+  }
+
+  const getX = idx => 400/width * (idx % 4);
+  const getY = idx => 300 / height * Math.floor(idx /4)
 
  return (
-    <div style={{display:'inline-grid' ,gridTemplateColumns:'repeat(4,'}}>
-      tiles.map({tile => <img src={images[tile]}/>})
+
+    <div className="App">
+      <svg viewBox='0 0 400 300'>
+        {tiles.map((t,idx) =>
+
+        <rect className='tile' style={{fill: TileMap[t]}} x={getX(idx)} y={getY(idx)} width={400/ width} height={300/height}></rect>
+)}
+
+          {path.map(p =>
+            <circle cs={p.x * 400/ width}  cy={p.y * 300 / height + (300 / height / 2)} r="10"  w/>
+            )}
+
+<circle className='player' style={{transform:`translate${playerX * 400 / width}px, ${playerY * 300 / height}`}} cx={400/width /2} cy={300/ height / 2} r="40" />
+      </svg>
     </div>
   ) 
 }
